28d2cc825be347659c85c86a4f0e917adbbcf466
David Almeida <davidalmeida154of@gmail.com>
2024-02-18 00:42:55 -0300

    feat: ensure SignUpController returns an error if no e-mail is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -18,4 +18,21 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('No name was provided'))
   })
+
+  test('Should return 400 if no e-mail is provided', () => {
+    const sut = new SignUpController()
+
+    const httpRequest = {
+      body: {
+        name: 'John Doe',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('No e-mail was provided'))
+  })
 })
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,16 @@
 export class SignUpController {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-explicit-any
-  handle(httpRequest: any) {
-    return {
-      statusCode: 400,
-      body: new Error('No name was provided'),
-    }
+  handle(httpRequest: any): any {
+    if (!httpRequest.body.name)
+      return {
+        statusCode: 400,
+        body: new Error('No name was provided'),
+      }
+
+    if (!httpRequest.body.email)
+      return {
+        statusCode: 400,
+        body: new Error('No e-mail was provided'),
+      }
   }
 }
