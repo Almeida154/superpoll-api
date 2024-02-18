be58201d399e100d494fca0836fa051fb2f12016
David Almeida <davidalmeida154of@gmail.com>
2024-02-18 01:55:34 -0300

    refactor: add Controller protocol

--- src/presentation/controllers/sign-up.ts
+++ src/presentation/controllers/sign-up.ts
@@ -1,8 +1,9 @@
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
+import { Controller } from '@/presentation/protocols/controller'
 import { badRequest } from '@/presentation/helpers/http'
 import { NoProvidedParamError } from '@/presentation/errors/no-provided-param'
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
