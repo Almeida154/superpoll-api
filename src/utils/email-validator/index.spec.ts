1d1999bcfe870c39bdbda82615b6f86628bcf767
David Almeida <davidalmeida154of@gmail.com>
2024-02-21 21:06:28 -0300

    test: ensure EmailValidator returns correct validator.js value

--- src/utils/email-validator/index.spec.ts
+++ src/utils/email-validator/index.spec.ts
@@ -1,11 +1,20 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vitest } from 'vitest'
+import validator from 'validator'
 
 import { EmailValidatorAdapter } from '.'
 
 describe('EmailValidatorAdapter', () => {
   it('Should returns false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
+    vitest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isEmailValid = sut.isValid('invalid@email.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  it('Should returns true if validator returns true', () => {
+    const sut = new EmailValidatorAdapter()
+    vitest.spyOn(validator, 'isEmail').mockReturnValueOnce(true)
+    const isEmailValid = sut.isValid('valid@email.com')
+    expect(isEmailValid).toBe(true)
   })
 })
