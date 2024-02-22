2d229c78ffe719543ae31d08e8c77000a8d8c75e
David Almeida <davidalmeida154of@gmail.com>
2024-02-21 21:12:01 -0300

    refactor: move EmailValidatorAdapter instance to a factory

--- src/utils/email-validator/index.spec.ts
+++ src/utils/email-validator/index.spec.ts
@@ -3,23 +3,27 @@ import validator from 'validator'
 
 import { EmailValidatorAdapter } from '.'
 
+const makeSUT = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
 describe('EmailValidatorAdapter', () => {
   it('Should returns false if validator returns false', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSUT()
     vitest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isEmailValid = sut.isValid('invalid@email.com')
     expect(isEmailValid).toBe(false)
   })
 
   it('Should returns true if validator returns true', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSUT()
     vitest.spyOn(validator, 'isEmail').mockReturnValueOnce(true)
     const isEmailValid = sut.isValid('valid@email.com')
     expect(isEmailValid).toBe(true)
   })
 
   it('Should calls validator with correct email', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSUT()
     const isEmailSpy = vitest.spyOn(validator, 'isEmail')
     sut.isValid('any@email.com')
     expect(isEmailSpy).toHaveBeenCalledWith('any@email.com')
