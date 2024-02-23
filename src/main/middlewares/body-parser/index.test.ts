0bf1d585148322e545fd3f50d6f8bae835edfab7
David Almeida <davidalmeida154of@gmail.com>
2024-02-23 17:09:46 -0300

    test: ensure requests parse body as json

---
+++ src/main/middlewares/body-parser/index.test.ts
@@ -0,0 +1,17 @@
+import { describe, it } from 'vitest'
+import request from 'supertest'
+
+import app from '@/main/config/app'
+
+describe('Body Parser Middleware', () => {
+  it('', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'any_name' })
+      .expect({ name: 'any_name' })
+  })
+})
