1d733c7914cf9e49b40ca3bfce52e1d5b7906624
David Almeida <davidalmeida154of@gmail.com>
2024-02-23 15:18:29 -0300

    refactor: change mongo-client helper to use vitest-mongodb

---
+++ src/global.d.ts
@@ -0,0 +1,6 @@
+/* eslint-disable no-var */
+declare module 'NodeJS' {
+  global {
+    var __MONGO_URI__: string
+  }
+}
--- src/infra/db/mongodb/helpers/mongo-client.ts
+++ src/infra/db/mongodb/helpers/mongo-client.ts
@@ -1,13 +1,34 @@
-import { MongoClient as Client } from 'mongodb'
+import { setup, teardown } from 'vitest-mongodb'
+import { MongoClient as Client, Collection } from 'mongodb'
+
+interface IConnectOptions {
+  useMemory?: boolean
+}
 
 export const MongoClient = {
   client: null as Client,
+  usingMemory: false,
 
-  async connect(uri: string): Promise<void> {
-    this.client = await MongoClient.connect(uri)
+  async connect(options?: IConnectOptions): Promise<void> {
+    if (options?.useMemory) {
+      return this.connectToMemoryServer()
+    }
+
+    this.client = await Client.connect(process.env.__MONGO_URI__)
+  },
+
+  async connectToMemoryServer(): Promise<void> {
+    await setup()
+    this.usingMemory = true
+    this.client = await Client.connect(globalThis.__MONGO_URI__)
   },
 
   async disconnect(): Promise<void> {
     await this.client.close()
+    if (this.usingMemory) teardown()
+  },
+
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name)
   },
 }
