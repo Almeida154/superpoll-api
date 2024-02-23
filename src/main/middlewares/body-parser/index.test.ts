1499480529da3529d3e83223229b661d050f3a48
David Almeida <davidalmeida154of@gmail.com>
2024-02-23 17:17:51 -0300

    test: ensure corsis enabled

--- src/main/middlewares/body-parser/index.test.ts
+++ src/main/middlewares/body-parser/index.test.ts
@@ -4,7 +4,7 @@ import request from 'supertest'
 import app from '@/main/config/app'
 
 describe('Body Parser Middleware', () => {
-  it('', async () => {
+  it('Should parses body as json', async () => {
     app.post('/test_body_parser', (req, res) => {
       res.send(req.body)
     })
---
+++ src/main/middlewares/cors/index.test.ts
@@ -0,0 +1,18 @@
+import { describe, it } from 'vitest'
+import request from 'supertest'
+
+import app from '@/main/config/app'
+
+describe('Cors Middleware', () => {
+  it('Should enable Cors', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send()
+    })
+
+    await request(app)
+      .get('/test_cors')
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*')
+  })
+})
