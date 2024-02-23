b4fdc57e8b6becae7e43a76b6dc07448755eae8e
David Almeida <davidalmeida154of@gmail.com>
2024-02-23 15:21:18 -0300

    test: ensure AccountMongoRepository.add returns and account

---
+++ src/infra/db/mongodb/repositories/account.spec.ts
@@ -0,0 +1,30 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { MongoClient } from '@/infra/db/mongodb/helpers/mongo-client'
+
+import { AccountMongoRepository } from './account'
+
+describe('Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoClient.connect({ useMemory: true })
+  })
+
+  afterAll(async () => {
+    await MongoClient.disconnect()
+  })
+
+  it('Should returns an account on success', async () => {
+    const sut = new AccountMongoRepository()
+
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email',
+      password: 'hashed_password',
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email')
+    expect(account.password).toBe('hashed_password')
+  })
+})
