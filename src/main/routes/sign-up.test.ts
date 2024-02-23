cd19cc0ba6fb3eb7340f80f633678301b81cb527
David Almeida <davidalmeida154of@gmail.com>
2024-02-23 18:13:40 -0300

    refactor: connect to mongodb before sign up integration tests

--- src/main/routes/sign-up.test.ts
+++ src/main/routes/sign-up.test.ts
@@ -1,9 +1,23 @@
-import { describe, it } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, it } from 'vitest'
 import request from 'supertest'
 
 import app from '@/main/config/app'
+import { MongoClient } from '@/infra/db/mongodb/helpers/mongo-client'
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await MongoClient.connect({ useMemory: true })
+  })
+
+  afterAll(async () => {
+    await MongoClient.disconnect()
+  })
+
+  beforeEach(async () => {
+    const collection = MongoClient.getCollection('accounts')
+    await collection.deleteMany()
+  })
+
   it('Should returns an account on success', async () => {
     await request(app)
       .post('/api/sign-up')
