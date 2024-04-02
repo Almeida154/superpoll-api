9e8bf8949b1f1c4abddec197580a8be470845959
David Almeida <davidalmeida154of@gmail.com>
2024-04-02 20:01:42 -0300

    feat: ensure express adaptRoute returns the error message instead of the error name

--- src/main/adapters/express-route.ts
+++ src/main/adapters/express-route.ts
@@ -3,11 +3,15 @@ import { IController, IHttpRequest } from '@/presentation/protocols'
 
 export const adaptRoute = (controller: IController) => {
   return async (req: Request, res: Response) => {
-    const httpRequest: IHttpRequest = {
+    const request: IHttpRequest = {
       body: req.body,
     }
 
-    const httpResponse = await controller.handle(httpRequest)
-    res.status(httpResponse.statusCode).send(httpResponse.body)
+    const response = await controller.handle(request)
+    const success = response.statusCode === 200
+
+    return res
+      .status(response.statusCode)
+      .send(success ? response.body : { error: response.body.message })
   }
 }
--- src/presentation/errors/internal-server-error.ts
+++ src/presentation/errors/internal-server-error.ts
@@ -1,6 +1,6 @@
 export class InternalServerError extends Error {
   constructor(stack: string) {
-    super('There was an error during the request')
+    super('There was an error during the request.')
     this.name = 'InternalServerError'
     this.stack = stack
   }
