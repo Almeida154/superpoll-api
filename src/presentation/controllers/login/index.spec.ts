5b8ca5eed89d248f59bd379ed8111fa7cb875624
David Almeida <davidalmeida154of@gmail.com>
2024-04-02 21:21:02 -0300

    test: ensure LoginController returns 400 if an invalid email is provided

--- src/presentation/controllers/login/index.spec.ts
+++ src/presentation/controllers/login/index.spec.ts
@@ -2,7 +2,7 @@ import { describe, expect, it, vi } from 'vitest'
 
 import { IEmailValidator, IHttpRequest } from '@/presentation/protocols'
 import { badRequest } from '@/presentation/helpers/http'
-import { NoProvidedParamError } from '@/presentation/errors'
+import { InvalidParamError, NoProvidedParamError } from '@/presentation/errors'
 
 import { LoginController } from '.'
 
@@ -67,6 +67,19 @@ describe('LoginController', () => {
     }
     await sut.handle(httpRequest)
     expect(isValidSpy).toHaveBeenCalledWith('any@email')
+  })
+
+  it('should return 400 if an invalid email is provided', async () => {
+    const { sut, emailValidatorStub } = makeSUT()
+    vi.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest: IHttpRequest = {
+      body: {
+        email: 'any@email',
+        password: 'any_password',
+      },
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
   })
 
   it('should return 404 if user not found', () => null)
