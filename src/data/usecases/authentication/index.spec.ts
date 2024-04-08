c5ac1d8031e851305069afec14500b826b616bd5
David Almeida <davidalmeida154of@gmail.com>
2024-04-08 20:36:36 -0300

    refactor: move tests instances to factories

--- src/data/usecases/authentication/index.spec.ts
+++ src/data/usecases/authentication/index.spec.ts
@@ -1,26 +1,45 @@
+/* eslint-disable prettier/prettier */
 import { describe, expect, it, vi } from 'vitest'
-import { AuthenticationUseCase } from '.'
 import { AccountModel } from '@/domain/models'
 import { ILoadAccountByEmailRepository } from '@/data/protocols'
+import { AuthenticationUseCase } from '.'
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'any_id',
+  email: 'any@email.com',
+  name: 'any_name',
+  password: 'any_password',
+})
+
+const makeLoadAccountByEmailRepository = (): ILoadAccountByEmailRepository => {
+  class LoadAccountByEmailRepositoryStub
+    implements ILoadAccountByEmailRepository {
+    async load(): Promise<AccountModel> {
+      return new Promise((resolve) => resolve(makeFakeAccount()))
+    }
+  }
+
+  return new LoadAccountByEmailRepositoryStub()
+}
+
+interface ISut {
+  sut: AuthenticationUseCase
+  loadAccountByEmailRepositoryStub: ILoadAccountByEmailRepository
+}
+
+const makeSut = (): ISut => {
+  const loadAccountByEmailRepositoryStub = makeLoadAccountByEmailRepository()
+  const sut = new AuthenticationUseCase(loadAccountByEmailRepositoryStub)
+
+  return {
+    sut,
+    loadAccountByEmailRepositoryStub,
+  }
+}
 
 describe('AuthenticationUseCase', () => {
   it('should call LoadAccountByEmailRepository with correct email', async () => {
-    class LoadAccountByEmailRepositoryStub
-      implements ILoadAccountByEmailRepository
-    {
-      async load(): Promise<AccountModel> {
-        const account: AccountModel = {
-          id: 'any_id',
-          email: 'any@email.com',
-          name: 'any_name',
-          password: 'any_password',
-        }
-        return new Promise((resolve) => resolve(account))
-      }
-    }
-    const loadAccountByEmailRepositoryStub =
-      new LoadAccountByEmailRepositoryStub()
-    const sut = new AuthenticationUseCase(loadAccountByEmailRepositoryStub)
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
     const loadSpy = vi.spyOn(loadAccountByEmailRepositoryStub, 'load')
     await sut.execute({ email: 'any@email.com', password: 'any_password' })
     expect(loadSpy).toHaveBeenCalledWith('any@email.com')
