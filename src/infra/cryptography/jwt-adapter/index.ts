956b4ec244bf404394f278f7b442dd8a7de548e1
David Almeida <davidalmeida154of@gmail.com>
2024-04-10 01:36:56 -0300

    feat: ensure JwtAdapter calls jwt sign with correct values

--- src/infra/cryptography/jwt-adapter/index.ts
+++ src/infra/cryptography/jwt-adapter/index.ts
@@ -1,3 +1,16 @@
-export class JwtAdapter {
-  //
+import jwt from 'jsonwebtoken'
+
+import { IEncrypter } from '@/data/protocols'
+
+export class JwtAdapter implements IEncrypter {
+  private readonly secret: string
+
+  constructor(secret: string) {
+    this.secret = secret
+  }
+
+  encrypt(id: string): Promise<string> {
+    jwt.sign({ id }, this.secret)
+    return null
+  }
 }
