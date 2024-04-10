56db04865a327aaac1cdca011220f57356e73871
David Almeida <davidalmeida154of@gmail.com>
2024-04-10 01:46:02 -0300

    refactor: move JwtAdapter instance to a factory

--- src/infra/cryptography/jwt-adapter/index.spec.ts
+++ src/infra/cryptography/jwt-adapter/index.spec.ts
@@ -11,22 +11,26 @@ vi.mock('jsonwebtoken', () => ({
   },
 }))
 
+const makeSut = () => {
+  return new JwtAdapter('secret')
+}
+
 describe('JwtAdapter', () => {
   it('should call sign with correct values', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     const signSpy = vi.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
   })
 
   it('should return a token if sign succeed', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('any_token')
   })
 
   it('should throw if sign throws', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSut()
     vi.spyOn(jwt, 'sign').mockImplementationOnce(() => {
       throw new Error()
     })
