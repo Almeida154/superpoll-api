e72c10fbd276752a4c6e71ae0c48ceeda89dda28
David Almeida <davidalmeida154of@gmail.com>
2024-04-16 21:39:18 -0300

    fix: inject AuthenticationUseCase dependency to SignUpController

--- src/main/factories/controllers/sign-up.ts
+++ src/main/factories/controllers/sign-up.ts
@@ -1,11 +1,13 @@
 import { SignUpController } from '@/presentation/controllers'
-import { AddAccountUseCase } from '@/data/usecases'
+import { AddAccountUseCase, AuthenticationUseCase } from '@/data/usecases'
 import { BcryptAdapter } from '@/infra/cryptography/bcrypt-adapter'
 import {
   AccountMongoRepository,
   LogMongoRepository,
 } from '@/infra/db/mongodb/repositories'
 import { IController } from '@/presentation/protocols'
+import { JwtAdapter } from '@/infra/cryptography'
+import env from '@/main/config/env'
 
 import { ControllerLogDecorator } from '../../decorators'
 import { makeSignUpValidation } from '../validations'
@@ -15,8 +17,19 @@ export const makeSignUpController = (): IController => {
   const accountRepository = new AccountMongoRepository()
   const addAccountUseCase = new AddAccountUseCase(accountRepository, encrypter)
 
+  const bcryptAdapter = new BcryptAdapter(12)
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+
+  const authenticationUseCase = new AuthenticationUseCase(
+    accountRepository,
+    accountRepository,
+    bcryptAdapter,
+    jwtAdapter,
+  )
+
   const signUpController = new SignUpController(
     addAccountUseCase,
+    authenticationUseCase,
     makeSignUpValidation(),
   )
 
