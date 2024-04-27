a9e0045f8ef6956cd600179df96ec64e0baa1750
David Almeida <davidalmeida154of@gmail.com>
2024-04-27 20:38:36 -0300

    test: ensure AddSurveyController calls AddSurveyUseCase

--- src/presentation/controllers/survey/add-survey/index.spec.ts
+++ src/presentation/controllers/survey/add-survey/index.spec.ts
@@ -5,8 +5,10 @@ import {
   IValidation,
 } from '../../authentication/sign-up/protocols'
 
-import { AddSurveyController } from '.'
 import { badRequest } from '@/presentation/helpers/http'
+import { IAddSurveyUseCase } from '@/domain/usecases/survey'
+
+import { AddSurveyController } from '.'
 
 const makeFakeRequest = (): IHttpRequest => ({
   body: {
@@ -30,18 +32,31 @@ const makeValidation = (): IValidation => {
   return new ValidationStub()
 }
 
+const makeAddSurveyUseCase = (): IAddSurveyUseCase => {
+  class AddSurveyUseCaseStub implements IAddSurveyUseCase {
+    execute(): Promise<void> {
+      return new Promise((resolve) => resolve())
+    }
+  }
+
+  return new AddSurveyUseCaseStub()
+}
+
 interface ISut {
   sut: AddSurveyController
   validationStub: IValidation
+  addSurveyUseCaseStub: IAddSurveyUseCase
 }
 
 const makeSut = (): ISut => {
   const validationStub = makeValidation()
-  const sut = new AddSurveyController(validationStub)
+  const addSurveyUseCaseStub = makeAddSurveyUseCase()
+  const sut = new AddSurveyController(validationStub, addSurveyUseCaseStub)
 
   return {
     sut,
     validationStub,
+    addSurveyUseCaseStub,
   }
 }
 
@@ -60,5 +75,13 @@ describe('AddSurveyController', () => {
     const httpRequest = makeFakeRequest()
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new Error()))
+  })
+
+  it('should call AddSurveyUseCase with correct values', async () => {
+    const { sut, addSurveyUseCaseStub } = makeSut()
+    const executeSpy = vi.spyOn(addSurveyUseCaseStub, 'execute')
+    const httpRequest = makeFakeRequest()
+    await sut.handle(httpRequest)
+    expect(executeSpy).toHaveBeenCalledWith(httpRequest.body)
   })
 })
