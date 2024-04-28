a079723165ff8b94fcabe35236cfd7e1197eaa49
David Almeida <davidalmeida154of@gmail.com>
2024-04-27 21:29:45 -0300

    test: ensure AddSurveyUseCase calls AddSurveyRepository

---
+++ src/data/usecases/add-survey/index.spec.ts
@@ -0,0 +1,45 @@
+import { describe, expect, it, vi } from 'vitest'
+import { IAddSurveyModel } from '@/domain/usecases/survey'
+
+import { AddSurveyUseCase } from '.'
+import { IAddSurveyRepository } from '@/data/protocols'
+
+const makeFakeSurvey = (): IAddSurveyModel => ({
+  answers: [{ answer: 'any_answer', image: 'any_image' }],
+  question: 'any_question',
+})
+
+const makeAddSurveyRepository = (): IAddSurveyRepository => {
+  class AddSurveyRepositoryStub implements IAddSurveyRepository {
+    add(): Promise<void> {
+      return new Promise((resolve) => resolve())
+    }
+  }
+
+  return new AddSurveyRepositoryStub()
+}
+
+interface ISut {
+  sut: AddSurveyUseCase
+  addSurveyRepositoryStub: IAddSurveyRepository
+}
+
+const makeSut = (): ISut => {
+  const addSurveyRepositoryStub = makeAddSurveyRepository()
+  const sut = new AddSurveyUseCase(addSurveyRepositoryStub)
+
+  return {
+    sut,
+    addSurveyRepositoryStub,
+  }
+}
+
+describe('AddSurveyUseCase', () => {
+  it('should call AddSurveyRepository with correct values', async () => {
+    const { sut, addSurveyRepositoryStub } = makeSut()
+    const addSpy = vi.spyOn(addSurveyRepositoryStub, 'add')
+    const survey = makeFakeSurvey()
+    await sut.execute(survey)
+    expect(addSpy).toHaveBeenCalledWith(survey)
+  })
+})
