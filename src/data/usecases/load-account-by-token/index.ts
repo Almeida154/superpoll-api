3d74bdddd6e135f6e3996f61234cac0cbf78bd5e
David Almeida <davidalmeida154of@gmail.com>
2024-06-07 12:58:01 -0300

    feat: ensure LoadAccountByTokenUseCase calls LoadAccountByTokenRepository with correct values

--- src/data/usecases/load-account-by-token/index.ts
+++ src/data/usecases/load-account-by-token/index.ts
@@ -1,12 +1,17 @@
+import { ILoadAccountByTokenRepository } from '@/data/protocols'
 import { IDecrypter } from '@/data/protocols/cryptography/decrypter'
 import { AccountModel } from '@/domain/models'
 import { ILoadAccountByTokenUseCase } from '@/domain/usecases/account'
 
 export class LoadAccountByTokenUseCase implements ILoadAccountByTokenUseCase {
-  constructor(private readonly decrypter: IDecrypter) {}
-  execute(accessToken: string, role?: string): Promise<AccountModel> {
-    console.log(accessToken, role)
-    this.decrypter.decrypt(accessToken)
-    return null
+  constructor(
+    private readonly decrypter: IDecrypter,
+    private readonly loadAccountByTokenRepository: ILoadAccountByTokenRepository,
+  ) {}
+
+  async execute(accessToken: string, role?: string): Promise<AccountModel> {
+    const token = await this.decrypter.decrypt(accessToken)
+    if (!token) return null
+    await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
   }
 }
