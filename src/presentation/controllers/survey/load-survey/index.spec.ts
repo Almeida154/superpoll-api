4a6c35f573cba93437e6cb3f1c2191af923fc7c6
David Almeida <davidalmeida154of@gmail.com>
2024-10-24 20:06:11 -0300

    test: ensure LoadSurveysController returns 500 if LoadSurveyUseCase throws

--- src/presentation/controllers/survey/load-survey/index.spec.ts
+++ src/presentation/controllers/survey/load-survey/index.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, it, vi } from 'vitest'
 import { LoadSurveysController } from '.'
 import { ILoadSurveysUseCase } from '@/domain/usecases/survey/load'
 import { SurveyModel } from '@/domain/models'
+import { internalException, ok } from '@/presentation/helpers/http'
 
 const makeFakeSurveys = () => [
   {
@@ -55,5 +56,20 @@ describe('LoadSurveysController', () => {
     const executeSpy = vi.spyOn(loadSurveysUseCaseStub, 'execute')
     await sut.handle()
     expect(executeSpy).toHaveBeenCalled()
+  })
+
+  it('should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle()
+    expect(httpResponse).toEqual(ok({ surveys: makeFakeSurveys() }))
+  })
+
+  it('should return 500 if LoadSurveysUseCase throws', async () => {
+    const { sut, loadSurveysUseCaseStub } = makeSut()
+    vi.spyOn(loadSurveysUseCaseStub, 'execute').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpResponse = await sut.handle()
+    expect(httpResponse).toEqual(internalException(new Error()))
   })
 })
